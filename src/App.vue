be1e11b9fa6ef628472a66be1609c287776633a1
LauraPeric <75144169+LauraPeric@users.noreply.github.com>
2023-08-06 01:47:04 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/login">Prijava</router-link> |
-    <router-link to="/signup">Registracija</router-link>
-  </nav>
-  <router-view/>
+  <div class="app-container">
+    <nav class="nav-container">
+      <img src="@/assets/facialcaremelogo.png" alt="Logo" class="logo" />
+      <div class="nav-links">
+        <router-link to="/">Naslovna stranica</router-link> |
+        <router-link to="/registracijaKiliA">Pridruži se</router-link> |
+        <router-link to="/info">Informacije</router-link>
+      </div>
+    </nav>
+    <router-view />
+  </div>
 </template>
+
 
 <style lang="scss">
 #app {
@@ -14,18 +20,50 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%; /* Puni ekran */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
-nav {
-  padding: 30px;
+/* Stil za navigaciju, logo i ostali sadržaj */
+.nav-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #f9dada;
+  padding: 10px 20px;
+  width: 100%; /* Puni ekran u širini */
+  box-sizing: border-box;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.logo {
+  width: 100px; /* Prilagoditi veličinu logotipa */
+  height: auto;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.nav-links {
+  display: flex;
+  gap: 20px;
+}
+
+a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+a.router-link-exact-active {
+  color: #42b983;
+}
+
+.app-container {
+  flex: 1; /* Ispuni preostali prostor */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center; /* Centriranje sadržaja vertikalno */
+  width: 100%; /* Puni ekran u širini */
+  box-sizing: border-box;
+  padding: 20px; /* Dodatno podešavanje razmaka */
 }
 </style>
