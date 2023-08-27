4d24d2043370951e4d06fb0d0558dcd5815e4c8c
LauraPeric <75144169+LauraPeric@users.noreply.github.com>
2023-08-27 16:54:55 +0200

    commit
    kartica za odabir itemsa

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="col-md-6 mb-4">
+    <div class="card">
+      <div class="card-body">
+        <div class="media">
+          <img
+            src="@/assets/MasnaMaskaGli.png"
+            class="mr-3"
+            alt="Slika 1"
+            width="160"
+          />
+          <div class="media-body">
+            <h5 class="card-title">Biofarm</h5>
+            <p class="card-text">
+              100% RAW glina s Mrtvog mora, 70 g
+              alkdnvpvdnsdčovisdnvčoisdhvnosdčivhnsdon vsivsodvij sdvjs vsimoč
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// OVO JE CARD I ZELIM NAPRAVIT 2.ROW 2 ITEMSA U
+// KOJI nisu usable vec sz za izgled
+
+export default {
+  name: "ItemCard",
+};
+</script>
